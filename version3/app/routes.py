72816793ea0d49a10ac3b611a85e41fff154f243
harry <harrisongitau3@gmail.com>
2021-12-13 18:36:40 +0300

    Fix: Manage each user separately

--- version3/app/routes.py
+++ version3/app/routes.py
@@ -1,47 +1,68 @@
 from app import app, db
 from flask import render_template, redirect, url_for, flash
-from app.forms import LoginForm, ParentRegistrationForm,\
-    StudentRegistrationForm, RequestPasswordResetForm,\
-    ResetPasswordForm
+from app.forms import LoginForm, ClientRegistrationForm,\
+    RequestPasswordResetForm, ResetPasswordForm
 from app.models import Client
 from flask_login import current_user, login_user, logout_user, login_required    
 
 
-@app.route('/')
-@app.route('/student-dashboard')
+@app.route('/student/dashboard')
 @login_required
-def student_dashboard():
-    return render_template('student_dashboard.html')
+def dashboard_student():
+    return render_template('dashboard_student.html')
 
 
-@app.route('/parent-dashboard')
+@app.route('/parent/dashboard')
 @login_required
-def parent_dashboard():
-    return render_template('parent_dashboard.html')
+def dashboard_parent():
+    return render_template('dashboard_parent.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/teacher/dashboard')
+@login_required
+def dashboard_teacher():
+    return render_template('dashboard_teacher.html')
+
+
+@app.route('/login')
 def login():
+    return render_template('login_users.html',
+                           title='Login'
+                           )
+
+
+@app.route('/parent/login', methods=['GET', 'POST'])
+def login_parent():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         parent = Client.query.filter_by(parent_email=form.email.data).first()
-        student = Client.query.filter_by(student_email=form.email.data).first()
         if parent is None or not parent.check_parent_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
-        elif student is None or not student.check_student_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        if parent:
-            login_user(parent, remember=form.remember_me.data)
-            return redirect(url_for('parent_dashboard'))
-        if student:
-            login_user(student, remember=form.remember_me.data)
-            return redirect(url_for('student_dashboard'))
+            return redirect(url_for('login_parent'))
+        login_user(parent, remember=form.remember_me.data)
+        return redirect(url_for('dashboard_parent'))
     return render_template('login.html',
-                           title='Log In',
+                           title='Parent Login',
+                           form=form
+                           )
+
+
+@app.route('/student/login', methods=['GET', 'POST'])
+def login_student():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        student = Client.query.filter_by(student_email=form.email.data).first()
+        if student is None or not student.check_student_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login_student'))
+        login_user(student, remember=form.remember_me.data)
+        return redirect(url_for('dashboard_student'))
+    return render_template('login.html',
+                           title='Student Login',
                            form=form
                            )
 
@@ -53,46 +74,30 @@ def logout():
 
 
 @app.route('/parent-registration', methods=['GET', 'POST'])
-def register_parent():
+def register_client():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    form = ParentRegistrationForm()
+    form = ClientRegistrationForm()
     if form.validate_on_submit():
-        parent = Client(parent_full_name=form.parent_full_name.data,
+        client = Client(parent_full_name=form.parent_full_name.data,
                         parent_email=form.parent_email.data,
                         parent_phone=form.parent_phone.data,
                         parent_occupation=form.parent_occupation.data,
                         parent_residence=form.parent_residence.data,
+                        student_full_name=form.student_full_name.data,
+                        student_email=form.student_email.data,
+                        student_phone=form.student_phone.data,
+                        student_school=form.student_school.data,
+                        student_age=form.student_age.data
                         )
-        parent.set_parent_password(form.parent_password.data)
-        db.session.add(parent)
+        client.set_parent_password(form.parent_password.data)
+        client.set_student_password(form.student_password.data)
+        db.session.add(client)
         db.session.commit()
-        flash('Parent successfully registered!')
-        return redirect(url_for('register_student'))
-    return render_template('register_parent.html',
-                           title='Parent Registration',
-                           form=form
-                           )
-
-
-@app.route('/student-registration', methods=['GET', 'POST'])
-def register_student():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = StudentRegistrationForm()
-    if form.validate_on_submit():
-        student = Client(student_full_name=form.student_full_name.data,
-                         student_email=form.student_email.data,
-                         student_phone=form.student_phone.data,
-                         student_school=form.student_school.data,
-                         student_age=form.student_age.data
-                         )
-        student.set_student_password(form.student_password.data)
-        db.session.add(student)
-        db.session.commit()
-        flash('Student successfully registered!')
-    return render_template('register_student.html',
-                           title='Student Registration',
+        flash('Parent and Student successfully registered. Login to continue!')
+        return redirect(url_for('login'))
+    return render_template('register_client.html',
+                           title='Client Registration',
                            form=form
                            )
 
