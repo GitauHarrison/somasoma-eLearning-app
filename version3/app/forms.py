0f3334012aebbe16c8e12dd2522fa13361d80ce8
harry <harrisongitau3@gmail.com>
2021-12-13 07:16:15 +0300

    Dev: Display registration and password reset forms

--- version3/app/forms.py
+++ version3/app/forms.py
@@ -1,3 +1,4 @@
+from app.models import Client
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -10,9 +11,88 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Sign In')
 
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+class ParentRegistrationForm(FlaskForm):
+    parent_full_name = StringField('Parent Full Name',
+                                   validators=[DataRequired(),
+                                               Length(min=2, max=20)
+                                               ]
+                                   )
+    parent_email = StringField('Parent Email',
+                               validators=[DataRequired(), Email()],
+                               render_kw={"placeholder": "Valid Email Address"}
+                               )
+    parent_phone = StringField('Parent Phone Number',
+                               validators=[DataRequired()]
+                               )
+    parent_occupation = StringField('Parent Occupation',
+                                    validators=[DataRequired()]
+                                    )
+    parent_residence = StringField('Parent Residence',
+                                   validators=[DataRequired()]
+                                   )
+    parent_password = PasswordField('Parent Password',
+                                    validators=[DataRequired()])
+    parent_confirm_password = PasswordField('Parent Confirm Password',
+                                            validators=[DataRequired(),
+                                                        EqualTo('parent_password')
+                                                        ]
+                                            )
+
+    submit = SubmitField('Register')
+
+    def validate_parent_full_name(self, parent_full_name):
+        client = Client.query.filter_by(parent_full_name=parent_full_name.data).first()
+        if client:
+            raise ValidationError('Name already taken. Please choose a different one.')
+
+    def validate_parent_email(self, parent_email):
+        client = Client.query.filter_by(parent_email=parent_email.data).first()
+        if client:
+            raise ValidationError('Name already taken. Please choose a different one.')
+
+
+class StudentRegistrationForm(FlaskForm):
+    student_full_name = StringField('Student Full Name',
+                                    validators=[DataRequired(),
+                                                Length(min=2, max=20)
+                                                ]
+                                    )
+    student_email = StringField('Student Email',
+                                validators=[DataRequired(), Email()],
+                                render_kw={"placeholder": "Valid Email Address"}
+                                )
+    student_phone = StringField('Student Phone Number',
+                                validators=[DataRequired()]
+                                )
+    student_school = StringField('Student School',
+                                 validators=[DataRequired()]
+                                 )
+    student_age = StringField('Student Age',
+                              validators=[DataRequired()]
+                              )
+    student_password = PasswordField('Student Password',
+                                     validators=[DataRequired()])
+    student_confirm_password = PasswordField('Student Confirm Password',
+                                             validators=[DataRequired(),
+                                                         EqualTo('parent_password')
+                                                         ]
+                                             )
+    submit = SubmitField('Register')
+
+    def validate_student_full_name(self, student_full_name):
+        client = Client.query.filter_by(student_full_name=student_full_name.data).first()
+        if client:
+            raise ValidationError('Name already taken. Please choose a different one.')
+
+    def validate_student_email(self, student_email):
+        client = Client.query.filter_by(student_email=student_email.data).first()
+        if client:
+            raise ValidationError('Name already taken. Please choose a different one.')
+
+
+class RequestPasswordResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()],
+                        render_kw={"placeholder": "Valid Email Address Used During Registration"}
+                        )
+    submit = SubmitField('Request Password Reset')
--- version3/app/routes.py
+++ version3/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, ParentRegistrationForm,\
+    StudentRegistrationForm, RequestPasswordResetForm
 
 
 @app.route('/')
@@ -12,10 +13,34 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Log In', form=form)
+    return render_template('login.html',
+                           title='Log In',
+                           form=form
+                           )
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    return render_template('register.html', title='Register', form=form)
+@app.route('/parent-registration', methods=['GET', 'POST'])
+def register_parent():
+    form = ParentRegistrationForm()
+    return render_template('register_parent.html',
+                           title='Parent Registration',
+                           form=form
+                           )
+
+
+@app.route('/student-registration', methods=['GET', 'POST'])
+def student_parent():
+    form = StudentRegistrationForm()
+    return render_template('register_student.html',
+                           title='Student Registration',
+                           form=form
+                           )
+
+
+@app.route('/request-password-reset', methods=['GET', 'POST'])
+def request_password_reset():
+    form = RequestPasswordResetForm()
+    return render_template('request_password_reset.html',
+                           title='Request Password Reset',
+                           form=form
+                           )
