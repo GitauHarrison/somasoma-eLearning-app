24a63261c4e7e7940ceb41f01ff0e0385fffd77c
harry <harrisongitau3@gmail.com>
2021-12-14 04:22:40 +0300

    Dev: Import twilio verify api in application instance

--- version3/app/__init__.py
+++ version3/app/__init__.py
@@ -14,4 +14,4 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models, errors
+from app import routes, models, errors, twilio_verify_api
