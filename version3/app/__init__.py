49781a3c2d469b05ed5da8962ebbb8498128d62b
harry <harrisongitau3@gmail.com>
2021-12-20 12:43:09 +0300

    Dev: Restrict access to parent and student dashboards

--- version3/app/__init__.py
+++ version3/app/__init__.py
@@ -23,8 +23,8 @@ bootstrap = Bootstrap(app)
 db = SQLAlchemy(app, metadata=metadata)
 migrate = Migrate(app, db, render_as_batch=True)
 login = LoginManager(app)
-login.login_view = 'login.student'
-login.login_view = 'login.parent'
+login.login_view = 'login_student'
+login.login_view = 'login_parent'
 moment = Moment(app)
 
 from app import routes, models, errors, twilio_verify_api
