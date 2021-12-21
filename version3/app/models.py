352f114717c98e6d7bbc2494d343befc3a2202bc
harry <harrisongitau3@gmail.com>
2021-12-22 02:55:58 +0300

    Dev: Get and verify password reset token

--- version3/app/models.py
+++ version3/app/models.py
@@ -3,6 +3,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 from hashlib import md5
+from flask import current_app
+import jwt
+from time import time
 
 
 @login.user_loader
@@ -111,6 +114,24 @@ class Student(UserMixin, db.Model):
             ).filter(followers.c.follower_id == self.id)
         own = CommunityComment.query.filter_by(student_id=self.id)
         return followed.union(own).order_by(CommunityComment.timestamp.desc())
+
+    # Password reset
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256'
+        ).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return Student.query.get(id)
 
 
 class Parent(UserMixin, db.Model):
