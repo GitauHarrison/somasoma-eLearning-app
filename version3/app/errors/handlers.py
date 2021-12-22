139395ac518a48851680668bdf77e2cff59bf452
harry <harrisongitau3@gmail.com>
2021-12-22 03:59:20 +0300

    Fix: Display error templates

--- version3/app/errors/handlers.py
+++ version3/app/errors/handlers.py
@@ -3,7 +3,7 @@ from flask import render_template
 from app.errors import bp
 
 
-@bp.errorhandler(404)
+@bp.app_errorhandler(404)
 def page_not_found(e):
     return render_template(
         'errors/404.html',
@@ -11,7 +11,7 @@ def page_not_found(e):
         ), 404
 
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_server_error(e):
     db.session.rollback()
     return render_template(
