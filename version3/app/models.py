dd5ae8cb827e50e0529ce411b5553ea6d535d9a3
harry <harrisongitau3@gmail.com>
2021-12-22 22:49:16 +0300

    Dev: Create anonymous user model and template inheritance model

--- version3/app/models.py
+++ version3/app/models.py
@@ -197,6 +197,30 @@ class Teacher(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
+# Anonymous User
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(120), index=True)
+    comment = db.Column(db.String(140))
+
+    comments = db.relationship(
+        'AnonymousTemplateInheritanceComment',
+        backref='author',
+        lazy='dynamic'
+        )
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
 
 class CommunityComment(db.Model):
     __tablename__ = 'community_comment'
@@ -264,4 +288,24 @@ class WebDevChapter1QuizOptions(db.Model):
 
 # ============================================================
 # END OF WEB DEVELOPMENT
+# ============================================================
+
+
+# ============================================================
+# ANONYMOUS COURSE CONTENT
+# ============================================================
+
+
+class AnonymousTemplateInheritanceComment(db.Model):
+    __tablename__ = 'template inheritance comment'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    student_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'Template Inheritance Comment: {self.body}'
+
+# ============================================================
+# ANONYMOUS COURSE CONTENT
 # ============================================================
