39e751f85f5e6c9a148847030ba4ce07040786b4
harry <harrisongitau3@gmail.com>
2021-12-26 05:27:40 +0300

    Fix: Import bp from admin instead of main blueprint

--- version3/app/admin/routes.py
+++ version3/app/admin/routes.py
@@ -1,8 +1,7 @@
 from app import db
-from app.main import bp
+from app.admin import bp
 from flask import render_template, redirect, url_for, flash, request,\
     current_app
-from app.main.forms import EditProfileForm, EmptyForm
 from app.models import CommunityComment, Admin
 from flask_login import current_user, login_required
 from datetime import datetime
