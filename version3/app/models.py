de76bff11458804cc439232b70a181fcdd67c30d
harry <harrisongitau3@gmail.com>
2021-12-27 02:50:37 +0300

    Dev: Allow for send of email within parent model

--- version3/app/models.py
+++ version3/app/models.py
@@ -218,6 +218,23 @@ class Parent(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
+    # Password reset
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256'
+        ).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return Parent.query.get(id)
+
 
 class Teacher(UserMixin, db.Model):
     __tablename__ = 'teacher'
