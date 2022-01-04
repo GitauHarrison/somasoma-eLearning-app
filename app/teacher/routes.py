d65bb9745a42598c190c6c02e402158a700dad94
harry <harrisongitau3@gmail.com>
2022-01-04 05:17:59 +0300

    Dev: Create new route for course management tab in teacher dashboard

--- app/teacher/routes.py
+++ app/teacher/routes.py
@@ -181,6 +181,129 @@ def dashboard_comment_moderation():
         )
 
 
+@bp.route('/dashboard/manage-course')
+@login_required
+def dashboard_manage_course():
+    teacher = Teacher.query.filter_by(
+        teacher_full_name=current_user.teacher_full_name).first()
+
+    # Manage Course Overview
+    course_overview_form = WebDevelopmentOverviewForm()
+    if course_overview_form.validate_on_submit():
+        course_overview = WebDevelopmentOverview(
+            title=course_overview_form.title.data,
+            overview=course_overview_form.body.data,
+            youtube_link=course_overview_form.youtube_link.data,
+        )
+        db.session.add(course_overview)
+        db.session.commit()
+        flash('Your course overview has been posted!', 'success')
+        return redirect(url_for('teacher.review_course_overview'))
+
+    # Manage Course Table of Contents
+    table_of_contents_form = TableOfContentsForm()
+    if table_of_contents_form.validate_on_submit():
+        table_of_contents = TableOfContents(
+            title=table_of_contents_form.title.data,
+            chapter=table_of_contents_form.chapter.data,
+            link=table_of_contents_form.link.data,
+        )
+        db.session.add(table_of_contents)
+        db.session.commit()
+        flash('Your table of contents has been updated!', 'success')
+        return redirect(url_for('teacher.review_table_of_contents'))
+
+    # Chapters
+    chapter_form = ChapterForm()
+    if chapter_form.validate_on_submit():
+        chapter = Chapter(
+            course=chapter_form.course.data,
+            chapter=chapter_form.chapter.data,
+            chapter_link=chapter_form.chapter_link.data,
+            chapter_review_link=chapter_form.chapter_review_link.data,
+            overview=chapter_form.overview.data,
+            accomplish=chapter_form.accomplish.data,
+            youtube_link=chapter_form.youtube_link.data,
+            conclusion=chapter_form.conclusion.data,
+            objective_1=chapter_form.objective_1.data,
+            objective_2=chapter_form.objective_2.data,
+            objective_3=chapter_form.objective_3.data,
+            objective_4=chapter_form.objective_4.data,
+            objective_5=chapter_form.objective_5.data,
+        )
+        db.session.add(chapter)
+        db.session.commit()
+        flash(f'{chapter} has been added!', 'success')
+        return redirect(url_for('teacher.review_chapters'))
+
+    # Chapter objectives
+    chapter_objectives_form = ChapterObjectivesForm()
+    if chapter_objectives_form.validate_on_submit():
+        chapter_objective = ChapterObjectives(
+            course=chapter_objectives_form.course.data,
+            chapter=chapter_objectives_form.chapter.data,
+            review_objectives_link=chapter_objectives_form.review_objectives_link.data,
+            objective_1=chapter_objectives_form.objective_1.data,
+            objective_2=chapter_objectives_form.objective_2.data,
+            objective_3=chapter_objectives_form.objective_3.data,
+            objective_4=chapter_objectives_form.objective_4.data,
+            objective_5=chapter_objectives_form.objective_5.data
+        )
+        db.session.add(chapter_objective)
+        db.session.commit()
+        flash('Chapter objectives have been added!', 'success')
+        return redirect(url_for('teacher.review_flask_objectives'))
+    all_objectives = ChapterObjectives.query.filter_by(
+        course=teacher.teacher_course).all()
+
+    # Chapter Quiz
+    chapter_quiz_form = ChapterQuizForm()
+    if chapter_quiz_form.validate_on_submit():
+        chapter_quiz = ChapterQuiz(
+            course=chapter_quiz_form.course.data,
+            chapter=chapter_quiz_form.chapter.data,
+            review_quiz_link=chapter_quiz_form.review_quiz_link.data,
+            quiz_1=chapter_quiz_form.quiz_1.data,
+            quiz_2=chapter_quiz_form.quiz_2.data,
+            quiz_3=chapter_quiz_form.quiz_3.data,
+            quiz_4=chapter_quiz_form.quiz_4.data,
+            quiz_5=chapter_quiz_form.quiz_5.data
+        )
+        db.session.add(chapter_quiz)
+        db.session.commit()
+        flash('Chapter quiz has been added!', 'success')
+        return redirect(url_for('teacher.review_chapter_quiz'))
+    all_quizzes = ChapterQuiz.query.filter_by(
+        course=teacher.teacher_course).all()
+
+    # Length
+    course_chapters = Chapter.query.filter_by(
+        course=teacher.teacher_course).all()
+    all_chapters = len(Chapter.query.all())
+    all_toc = len(TableOfContents.query.all())
+    all_course_overview = len(WebDevelopmentOverview.query.all())
+    all_chapter_objectives = len(ChapterObjectives.query.all())
+    all_chapter_quizzes = len(ChapterQuiz.query.all())
+    return render_template(
+        'teacher/manage_course.html',
+        title='Manage Course',
+        teacher=teacher,
+        course_chapters=course_chapters,
+        all_chapters=all_chapters,
+        all_toc=all_toc,
+        all_course_overview=all_course_overview,
+        all_chapter_objectives=all_chapter_objectives,
+        all_chapter_quizzes=all_chapter_quizzes,
+        course_overview_form=course_overview_form,
+        table_of_contents_form=table_of_contents_form,
+        chapter_form=chapter_form,
+        chapter_objectives_form=chapter_objectives_form,
+        chapter_quiz_form=chapter_quiz_form,
+        all_objectives=all_objectives,
+        all_quizzes=all_quizzes
+        )
+
+
 @bp.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard_teacher():
