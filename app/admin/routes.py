7d594e63860ba361487bc9a1cc0a9bb653c17812
harry <harrisongitau3@gmail.com>
2022-01-12 06:30:58 +0300

    Fix(admin): Remove duplicate code and template

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -73,7 +73,7 @@ def dashboard_all_teachers():
         db.session.commit()
         send_registration_details_teacher(teacher)
         flash('Teacher successfully registered')
-        return redirect(url_for('admin.dashboard_admin'))
+        return redirect(url_for('admin.dashboard_all_teachers'))
     # ---------------------
     # End of teacher registration
     # ---------------------
@@ -147,7 +147,10 @@ def dashboard_courses_offered():
         db.session.add(course)
         db.session.commit()
         flash('Your course has been updated. Take action now!')
-        return redirect(url_for('admin.dashboard_admin', _anchor='courses'))
+        return redirect(url_for(
+            'admin.dashboard_courses_offered',
+            _anchor='courses')
+            )
 
     page = request.args.get('page', 1, type=int)
     courses = Courses.query.order_by(
@@ -158,12 +161,12 @@ def dashboard_courses_offered():
             False
             )
     next_url = url_for(
-        'admin.dashboard_admin',
+        'admin.dashboard_courses_offered',
         page=courses.next_num,
         _anchor="courses") \
         if courses.has_next else None
     prev_url = url_for(
-        'admin.dashboard_admin',
+        'admin.dashboard_courses_offered',
         page=courses.prev_num,
         _anchor="courses") \
         if courses.has_prev else None
@@ -258,12 +261,12 @@ def dashboard_student_stories():
             False
             )
     next_url = url_for(
-        'admin.dashboard_admin',
+        'admin.dashboard_student_stories',
         page=courses.next_num,
         _anchor="courses") \
         if courses.has_next else None
     prev_url = url_for(
-        'admin.dashboard_admin',
+        'admin.dashboard_student_stories',
         page=courses.prev_num,
         _anchor="courses") \
         if courses.has_prev else None
@@ -274,179 +277,6 @@ def dashboard_student_stories():
         admin=admin,
         next_url=next_url,
         prev_url=prev_url
-        )
-
-
-@bp.route('/dashboard', methods=['GET', 'POST'])
-@login_required
-def dashboard_admin():
-    admin = Admin.query.filter_by(
-        admin_full_name=current_user.admin_full_name
-        ).first()
-
-    students = Student.query.order_by(Student.student_last_seen.desc()).all()
-    teachers = Teacher.query.order_by(Teacher.teacher_last_seen.desc()).all()
-    parents = Parent.query.order_by(Parent.parent_last_seen.desc()).all()
-    all_students = len(Student.query.all())
-    all_teachers = len(Teacher.query.all())
-    all_parents = len(Parent.query.all())
-
-    # ---------------------
-    # Teacher Registration
-    # ---------------------
-    teacher_form = TeacherRegistrationForm()
-    if teacher_form.validate_on_submit():
-        teacher = Teacher(
-            teacher_full_name=teacher_form.teacher_full_name.data,
-            teacher_email=teacher_form.teacher_email.data,
-            teacher_phone=teacher_form.teacher_phone.data,
-            teacher_residence=teacher_form.teacher_residence.data,
-            teacher_course=teacher_form.teacher_course.data
-        )
-        teacher.set_password(teacher_form.teacher_password.data)
-        db.session.add(teacher)
-        db.session.commit()
-        send_registration_details_teacher(teacher)
-        flash('Teacher successfully registered')
-        return redirect(url_for('admin.dashboard_admin'))
-    # ---------------------
-    # End of teacher registration
-    # ---------------------
-
-    # ----------------
-    # Course Offering
-    # ----------------
-    course_form = CoursesForm()
-    if course_form.validate_on_submit():
-        course = Courses(
-            title=course_form.title.data,
-            body=course_form.body.data,
-            overview=course_form.overview.data,
-            next_class_date=course_form.next_class_date.data,
-            link=course_form.link.data
-            )
-
-        # Handling file upload
-        uploaded_file = course_form.course_image.data
-        filename = secure_filename(uploaded_file.filename)
-        if not os.path.exists(current_app.config['UPLOAD_PATH']):
-            os.makedirs(current_app.config['UPLOAD_PATH'])
-        course_image_path = os.path.join(
-            current_app.config['UPLOAD_PATH'],
-            filename
-            )
-        print('Img path:', course_image_path)
-        uploaded_file.save(course_image_path)
-        course.course_image = course_image_path
-        print('Db path: ', course.course_image)
-
-        course_image_path_list = course.course_image.split('/')[1:]
-        print('Img path list: ', course_image_path_list)
-        new_course_image_path = '/'.join(course_image_path_list)
-        print('New img path: ', new_course_image_path)
-        course.course_image = new_course_image_path
-        print(course.course_image)
-
-        db.session.add(course)
-        db.session.commit()
-        flash('Your course has been updated. Take action now!')
-        return redirect(url_for('admin.dashboard_admin', _anchor='courses'))
-
-    page = request.args.get('page', 1, type=int)
-    courses = Courses.query.order_by(
-        Courses.timestamp.desc()
-        ).paginate(
-            page,
-            current_app.config['POSTS_PER_PAGE'],
-            False
-            )
-    next_url = url_for(
-        'admin.dashboard_admin',
-        page=courses.next_num,
-        _anchor="courses") \
-        if courses.has_next else None
-    prev_url = url_for(
-        'admin.dashboard_admin',
-        page=courses.prev_num,
-        _anchor="courses") \
-        if courses.has_prev else None
-    all_courses = len(Courses.query.all())
-
-    # ----------------
-    # End of Course Offering
-    # ----------------
-
-    # ----------------
-    # Blogs: anonymous user
-    # ----------------
-
-    blog_articles_form = BlogArticlesForm()
-    if blog_articles_form.validate_on_submit():
-        blog_articles = BlogArticles(
-            article_image=blog_articles_form.article_image.data,
-            article_name=blog_articles_form.article_name.data,
-            body=blog_articles_form.body.data,
-            link=blog_articles_form.link.data
-        )
-
-        # Handling file upload
-        uploaded_file = blog_articles_form.article_image.data
-        filename = secure_filename(uploaded_file.filename)
-        if not os.path.exists(current_app.config['UPLOAD_PATH']):
-            os.makedirs(current_app.config['UPLOAD_PATH'])
-        blog_image_path = os.path.join(
-            current_app.config['UPLOAD_PATH'],
-            filename
-            )
-        print('Img path:', blog_image_path)
-        uploaded_file.save(blog_image_path)
-        blog_articles.article_image = blog_image_path
-        print('Db path: ', blog_articles.article_image)
-
-        blog_image_path_list = blog_articles.article_image.split('/')[1:]
-        print('Img path list: ', blog_image_path_list)
-        new_blog_image_path = '/'.join(blog_image_path_list)
-        print('New img path: ', new_blog_image_path)
-        blog_articles.article_image = new_blog_image_path
-        print(blog_articles.article_image)
-
-        db.session.add(blog_articles)
-        db.session.commit()
-        flash('You have addeded a new blog article')
-        return redirect(url_for('admin.dashboard_admin'))
-    all_blog_articles = len(BlogArticles.query.all())
-
-    # ----------------
-    # End of Blogs: anonymous user
-    # ----------------
-
-    return render_template(
-        'admin/dashboard_admin.html',
-        title='Admin Dashboard',
-        admin=admin,
-
-        # All users
-        students=students,
-        teachers=teachers,
-        parents=parents,
-
-        all_students=all_students,
-        all_teachers=all_teachers,
-        all_parents=all_parents,
-
-        # Forms
-        teacher_form=teacher_form,
-        course_form=course_form,
-
-        # Courses
-        courses=courses.items,
-        next_url=next_url,
-        prev_url=prev_url,
-        all_courses=all_courses,
-
-        # Blog
-        blog_articles_form=blog_articles_form,
-        all_blog_articles=all_blog_articles
         )
 
 
@@ -465,11 +295,11 @@ def profile_admin(admin_full_name):
         ).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for(
-        'admin.dashboard_admin', admin_full_name=admin_full_name,
+        'admin.profile_admin', admin_full_name=admin_full_name,
         page=comments.next_num) \
         if comments.has_next else None
     prev_url = url_for(
-        'admin.dashboard_admin', admin_full_name=admin_full_name,
+        'admin.profile_admin', admin_full_name=admin_full_name,
         page=comments.prev_num) \
         if comments.has_prev else None
     return render_template(
@@ -496,7 +326,7 @@ def edit_profile_admin():
         current_user.admin_about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('admin.dashboard_admin'))
+        return redirect(url_for('admin.profile_admin'))
     elif request.method == 'GET':
         form.email.data = current_user.admin_email
         form.about_me.data = current_user.admin_about_me
@@ -520,7 +350,7 @@ def delete_account_student(student_id):
     db.session.delete(student)
     db.session.commit()
     flash(f'Student {student_id} account has been deleted!')
-    return redirect(url_for('admin.dashboard_admin'))
+    return redirect(url_for('admin.dashboard_all_students'))
 
 
 @bp.route('/teacher/<teacher_id>/delete-account')
@@ -531,7 +361,7 @@ def delete_account_teacher(teacher_id):
     db.session.delete(teacher)
     db.session.commit()
     flash(f'Teacher {teacher_id} account has been deleted!')
-    return redirect(url_for('admin.dashboard_admin'))
+    return redirect(url_for('admin.dashboard_all_teachers'))
 
 
 @bp.route('/parent/<parent_id>/delete-account')
@@ -542,7 +372,7 @@ def delete_account_parent(parent_id):
     db.session.delete(parent)
     db.session.commit()
     flash(f'Parent {parent_id} account has been deleted!')
-    return redirect(url_for('admin.dashboard_admin'))
+    return redirect(url_for('admin.dashboard_all_parents'))
 
 # ==========================================
 # END OF DELETE USERS ACCOUNT
@@ -553,7 +383,7 @@ def delete_account_parent(parent_id):
 # ==========================================
 
 
-@bp.route('/admin/courses/<course_id>/delete')
+@bp.route('/courses/<course_id>/delete')
 def delete_course(course_id):
     course = Courses.query.filter_by(
         id=course_id
@@ -561,10 +391,13 @@ def delete_course(course_id):
     db.session.delete(course)
     db.session.commit()
     flash(f'Course {course_id} has been deleted!')
-    return redirect(url_for('admin.dashboard_admin', _anchor='courses'))
+    return redirect(url_for(
+        'admin.dashboard_courses_offered',
+        _anchor='courses')
+        )
 
 
-@bp.route('/admin/courses/<course_id>/allow', methods=['GET', 'POST'])
+@bp.route('/courses/<course_id>/allow', methods=['GET', 'POST'])
 def allow_course(course_id):
     course = Courses.query.filter_by(
         id=course_id
@@ -572,7 +405,10 @@ def allow_course(course_id):
     course.allowed_status = True
     db.session.commit()
     flash(f'Course {course_id} has been authorized!')
-    return redirect(url_for('admin.dashboard_admin', _anchor="courses"))
+    return redirect(url_for(
+        'admin.dashboard_courses_offered',
+        _anchor="courses")
+        )
 
 
 # ==========================================
@@ -627,7 +463,7 @@ def delete_blog_article(blog_article_id):
     db.session.delete(blog_article)
     db.session.commit()
     flash(f'Blog article {blog_article_id} has been deleted')
-    return redirect(url_for('admin.dashboard_admin'))
+    return redirect(url_for('admin.review_blog_articles'))
 
 
 @bp.route('/blog/articles/<blog_article_id>/allow')
@@ -638,7 +474,7 @@ def allow_blog_article(blog_article_id):
     blog_article.allowed_status = True
     db.session.commit()
     flash(f'Blog article {blog_article_id} has been authorized')
-    return redirect(url_for('admin.dashboard_admin'))
+    return redirect(url_for('admin.review_blog_articles'))
 
 # ==========================================
 # END OF MANAGE BLOG POSTS
